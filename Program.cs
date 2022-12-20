44e65c3b20f2883ef3a51133f0056d8f15926132
Oleg Iraklionov <irakliov@mail.ru>
2022-12-20 16:48:38 +0300

    Second case

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу,которая на вход принимает два числа 
+// и выдает какое число большее, а какое меньшее
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if (number1>=number2)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else
+{
+    Console.WriteLine($"max = {number2}");
+}
